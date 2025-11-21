e8f7ba57f58dd546e88e2e69be2673205f07cb0f
ArthurPuron <arthurpuron@gmail.com>
2025-11-20 22:03:27 -0300

    troca os iconoes pelos pngs

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,8 @@ import {
 import claveLogo from './assets/clave.png';
 import rodaLogo from './assets/roda.png'; // (Assumindo que o nome do arquivo da roda é 'roda.png')
 import loginLogo from './assets/logo-login.png';
+import wazeLogo from './assets/waze.png';
+import mapsLogo from './assets/maps.png';
 
 /*
   LEIA ANTES DE RODAR: INSTRUÇÕES DO IMPLEMENTADOR (Passo 36)
@@ -880,6 +882,7 @@ const ViewEventModal = ({ evento, onClose, userRole, userEmail }) => {
         </div>
       </div>
 
+      {/* Modal de Escolha de GPS */}
       {showMapModal && (
         <div 
           className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-60 backdrop-blur-sm"
@@ -907,12 +910,8 @@ const ViewEventModal = ({ evento, onClose, userRole, userEmail }) => {
                 onClick={() => setShowMapModal(false)}
                 title="Google Maps"
               >
-                <svg className="w-10 h-10" viewBox="0 0 24 24" fill="currentColor">
-                  <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
-                  <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
-                  <path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/>
-                  <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
-                </svg>
+                {/* AQUI: Usando a imagem PNG do Maps */}
+                <img src={mapsLogo} alt="Google Maps" className="w-10 h-10 object-contain" />
               </a>
               
               <a 
@@ -923,9 +922,8 @@ const ViewEventModal = ({ evento, onClose, userRole, userEmail }) => {
                 onClick={() => setShowMapModal(false)}
                 title="Waze"
               >
-                <svg className="w-10 h-10 text-[#F5F0ED]" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M19.1 11.8C18.6 7.5 15.4 4 11.5 4 6.8 4 3 7.8 3 12.5c0 2.1.8 4 2.1 5.5C4.2 18.6 3.5 19.5 3.5 20.5c0 1.4 1.1 2.5 2.5 2.5 1 0 1.9-.6 2.3-1.5 1 .3 2.1.5 3.3.5 1.1 0 2.2-.2 3.2-.5.4.9 1.3 1.5 2.3 1.5 1.4 0 2.5-1.1 2.5-2.5 0-1-.8-2-1.7-2.3 1.2-1.5 1.8-3.4 1.8-5.4 0-.3 0-.6-.1-.9zM6 21.5c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1zm5.5-5c-1.9 0-3.5-1.6-3.5-3.5S9.6 9.5 11.5 9.5s3.5 1.6 3.5 3.5-1.6 3.5-3.5 3.5zm6 5c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1z"/>
-                </svg>
+                {/* AQUI: Usando a imagem PNG do Waze */}
+                <img src={wazeLogo} alt="Waze" className="w-10 h-10 object-contain" />
               </a>
             </div>
           </div>
